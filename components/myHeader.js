871d77ee1d1231ee211adbb2627e01d0793b041c
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-03-28 09:16:18 -0500

    build: :construction: Parte 1 Worker & LocalStorage
    Empezamos a trabajar los datos desde el local storage cargandolos en segundo plano con el worker (aun no realizado el worker ni el local storage)

--- components/myHeader.js
+++ components/myHeader.js
@@ -3,7 +3,6 @@ export default {
         name: "Mojang",
         href: "index.html"
     },
-    boton: "OBTENER MINECRAFT",
     games: [
     {
         name: "Minecraft",
@@ -26,17 +25,26 @@ export default {
         href: "#"
     },
     ],
-    listTitle(){
-        document.querySelector("#title").insertAdjacentHTML("beforeend", `<a class="blog-header-logo" href="${this.title.href}">${this.title.name}</a>`)
-    },
-    listBoton(){
-        document.querySelector("#botonNav")
-    },
-    listarGames(){
-        let plantilla = "";
-        this.games.forEach((val, id) => {
-            plantilla += `<a class="p-2" href="${val.href}">${val.name}</a>`    
-        });
-        document.querySelector("#games").insertAdjacentHTML("beforeend", plantilla);
+    worker(){
+        //Creamos el worker
+        const ws = new Worker("storage/wsMyHeader.js", {type: "module"});
+        //enviamos un mensaje al worker
+        let id = [];
+        let count = 0;
+        //Aqui metemos title en la variable id
+        ws.postMessage({module: "listTitle", data: this.title});
+        //Aqui metemos games en la variable id
+        ws.postMessage({module: "listarGames", data: this.games});
+        id=["#title", "#games"];
+
+        //Aqui recibimos el mensaje del worker
+        ws.addEventListener("message", (e)=>{
+            //recorremos o parseamos lo que trae el evento mensaje
+            let doc = new DOMParser().parseFromString(e.data, "text/html");
+            //metemos en el index en el selector
+            document.querySelector(id[count]).append(...doc.body.children);
+            //terminamos el proceso del worker
+            (id.length-1==count) ? ws.terminate(): count++;
+        })
     }
 };
---
+++ storage/config.js
@@ -0,0 +1,9 @@
+export default {
+
+}
+
+//importamos todos los objetos desde aqui hacia el local storage
+
+//usaremos los objetos desde el local storage para meterlos en el worker y luego a las funciones asignadas
+
+//importamos esto a cada uno
